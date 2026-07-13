1cca5e54b08e14b16762cc7e4dd2b32736a762cb
piyush kumar <simplelogin-newsletter.9i6tu@aleeas.com>
2026-07-13 16:08:24 +0530

    added overriding

---
+++ JavaOOPproject/src/com/rays/oop/overriding/bank/Sbi.java
@@ -0,0 +1,6 @@
+package com.rays.oop.overriding.bank;
+
+public class Sbi extends Rbi {
+	
+	
+}
---
+++ JavaOOPproject/src/com/rays/oop/overriding/bank/TestRbi.java
@@ -0,0 +1,31 @@
+package com.rays.oop.overriding.bank;
+
+public class TestRbi {
+
+	public static void main(String[] args) {
+		System.out.println("---axis bank---");
+		Axis a = new Axis();
+
+		a.setInterestRateAxis(11.1);
+		System.out.println("Interest rate of Axis bank : " + a.getInterestRateAxis() + "%");
+
+		System.out.println();
+
+		System.out.println("---icici bank---");
+
+		Icici i = new Icici();
+
+		i.setInterestRateIcici(9.8);
+		System.out.println("Interest rate of Icici bank :" + i.getInterestRateIcici() + "%");
+
+		System.out.println();
+
+		System.out.println("---sbi bank---");
+
+		Sbi s = new Sbi();
+
+		System.out.println("Interest rate of Sbi bank : " + s.getInterestRate() + "%");
+
+	}
+
+}
